8156e50cb34a6d3498cdc803bf5c55196e54c8cf
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-17 15:15:05 -0500

    Fixed bug with account and card

--- mailoperation.py
+++ mailoperation.py
@@ -4,188 +4,6 @@ from utils import regexp_in_list, convert_money, operation_types, get_category
 from db_sql import DB
 from dotenv import dotenv_values
 
-# class MailOperation:
-#     def __init__(self, email_body, date):
-#         self.email_body = email_body
-#         self.date = date
-#         self.soup = BeautifulSoup(self.email_body, "html.parser")
-#         self.all_cards = DB().all_cards()
-#         self.all_accounts = DB().all_accounts()
-#         self.operation = self.generate_output(self.date)
-
-#     def is_valid(self):
-#         if self.op_type() is not None:
-#             if self.op_entity() is not None:
-#                 if self.op_card() is not None:
-#                     if self.op_amount() is not None:
-#                         return True
-#         print(f"Skipping mail from {self.date} because it's not a recognized operation")
-#         return False
-
-#     def generate_output(self, date):
-#         if self.is_valid():
-#             operation = {}
-#             operation['datetime'] = date
-#             operation['card'] = self.op_card()
-#             operation['type'] = self.op_type()
-#             operation['amount'] = convert_money(self.op_amount())
-#             operation['entity'] = self.op_entity()
-#             if get_category(self.op_entity()) is not None:
-#                 operation['category'] = get_category(self.op_entity())
-#             else:
-#                 operation['category'] = ''
-#             print(operation)
-#             return operation
-
-#     def op_entity(self):
-#         tables = self.soup.find_all('td')
-#         entity = None
-
-#         for item in tables:
-#             for op_k, op_v in operation_types().items():
-#                 for value in op_v:
-#                     if value in item.text:
-#                         operation_string = value.lower()
-
-#         for item in tables:
-#             testing = item.text
-#             if self.op_type() == 'bank payment':                                        # cobro de tarjeta
-#                 text_wo_money = item.text.lower().split(self.op_amount())[1].split('abonar deuda en Pesos.')[1]
-#                 # string = text_wo_money.split('abonar deuda en Pesos.')[1]
-#                 for key in self.all_cards:
-#                     new_key = key.replace('\\', '')
-#                     if new_key in text_wo_money:
-#                         string = text_wo_money.split(new_key)[0].strip(' ')
-#                         break
-
-#             elif re.search("0180*931987", item.text):   # This leaves out one case
-#                 if self.op_type() == 'manual payment':                                        # pago manual
-#                     text_w_money = item.text.lower().split(operation_string)[1]
-#                     string = text_w_money.split('realizo abono')[0].strip()
-#                 elif self.op_type() == 'transfer':                                          # transferencia
-#                     text_wo_money = item.text.lower().split(self.op_amount())[1]
-#                     # print(f"string: {text_wo_money}")
-#                     # print(f"money: {money}")
-#                     # print(f"account: {account}")
-#                     date = re.search("\d{2}/\d{2}/\d{4}", text_wo_money).group()
-#                     # print(f"date: {date}")
-#                     for account in self.all_accounts:
-#                         # for key, value in card:
-#                         new_key = account['number']
-#                             # new_key = key.replace('\\', '')
-#                         if new_key in text_wo_money:
-#                             string = text_wo_money.split(new_key)[1].split(date)[0].replace(' a ', '').replace('.', '')
-#                             break
-#                 elif self.op_type() == 'extraction':                                        # extracciones
-#                     text_w_money = item.text.lower().split(operation_string)[1]
-#                     if re.search("\d{2}\:\d{2}", text_w_money):
-#                         text_w_timedate = text_w_money.split(self.op_amount())[1]
-#                         time = re.search("\d{2}\:\d{2}", text_w_timedate).group()
-#                         string = text_w_timedate.split(time)[0].strip('en ').lstrip().replace('. hora', '')
-#                 elif self.op_type() == 'expense':
-#                     # print(item.text.lower())
-#                     text_w_money = item.text.lower().split(operation_string)[1]
-#                     if re.search("\d{2}\:\d{2}", text_w_money):
-#                         if 'desde cta' in text_w_money:
-#                             text_w_timedate = text_w_money.split(self.op_amount())[1]         # pagos (a veces son debitos automaticos)
-#                             string = text_w_timedate.replace(" a ", "").split(" desde cta")[0]
-#                         else:
-#                             text_w_timedate = text_w_money.split(self.op_amount())[1]         # compras habituales
-#                             time = re.search("\d{2}\:\d{2}", text_w_timedate).group()
-#                             string = text_w_timedate.split(time)[0].strip('en ').lstrip()
-#                 elif self.op_type() == 'recurring bill':
-#                     text_w_money = item.text.lower().split(operation_string)[1]
-#                     if re.search("\d{2}/\d{2}/\d{4}", text_w_money):               # debito automatico (no time, only date)
-#                         text_no_extras = text_w_money.split(self.op_amount())[0].strip('por ')
-#                         text_no_extras = text_no_extras.split(' (')[0]
-#                         string = text_no_extras
-#                 else:
-#                     print(self.op_type())
-#                     print(testing)
-#                     raise ValueError("Could not find time nor date to grab phrase")
-#                 entity = string.title()
-#         if entity is None:
-#             print("Operation place/person could not be found using known method as reference")
-#             return None
-#             # raise ValueError("Operation place/person could not be found using known method as reference")
-#         else:
-#             return entity
-
-#     def op_card(self):
-#         tables = self.soup.find_all('td')
-#         # item = regexp_in_list(self.accounts, tables)
-#         # cards = DB().all_cards()
-#         for card in DB().all_cards():
-#             for element in tables:
-#                 if re.search("0180*931987", element.text):
-#                     if card['number'] in element.text:
-#                         return card['id']
-#         for account in DB().all_accounts():
-#             for element in tables:
-#                 if re.search("0180*931987", element.text):
-#                     if account['number'] in element.text:
-#                         return account['id']
-#         else:
-#             print("Operation card/account could not be found using known card/account numbers as reference")
-#             return None
-
-#         # item = regexp_in_list(cards, tables)
-#         # if self.op_type() == 'bank payment':
-#         #     item = 'ahorro marina'
-#         # if item is None:
-#         #     print("Operation account could not be found using known card numbers as reference")
-#         #     return None
-#         #     # raise ValueError("Operation account could not be found using known card numbers as reference")
-#         # else:
-#         #     return item
-
-#     def op_type(self):
-#         for item in self.soup.find_all('td'):
-#             for op_k, op_v in operation_types().items():
-#                 for value in op_v:
-#                     if value in item.text:
-#                         return op_k
-#         if item is None:
-#             print("Operation type could not be found using known phrases as reference")
-#             return None
-#         else:
-#             return item
-
-#     # def op_amount(self):
-#     #     tables = self.soup.find_all('td')
-#     #     money = None
-#     #     for item in tables:
-#     #         if re.search("0180*931987", item.text):
-#     #             try:
-#     #                 money = re.search("(?:[$])(?:\d+(?:[.,]?\d*)*)", item.text).group()
-#     #             except AttributeError:
-#     #                 return None
-#     #     if money is None:
-#     #         print(f"Operation amount {money} could not be found using phone number as reference")
-#     #         return None
-#     #         # raise ValueError(f"Operation amount {money} could not be found using phone number as reference")
-#     #     else:
-#     #         return money
-#     def op_amount(self):
-#         tables = self.soup.find_all('td')
-#         money = None
-#         for item in tables:
-#             money = re.search("(?:[$])(?:\d+(?:[.,]?\d*)*)", item.text)
-#             if money:
-#                 return money.group()
-#         else:
-#             print(f"Operation amount {money} could not be found using phone number as reference")
-#             return None
-
-
-
-
-
-
-
-
-
-
 
 class MailOperation:
     def __init__(self, email_body, date):
@@ -237,12 +55,7 @@ class MailOperation:
                 for value in op_v:
                     if value.lower() in item.text.lower():
                         return op_k
-        # if item is None:
-            # print("Operation type could not be found using known phrases as reference")
-            # print(f"Mail string: {}")
         return None
-        # else:
-        #     return item
 
     def op_amount(self):
         money = None
@@ -251,34 +64,38 @@ class MailOperation:
             if money:
                 return money.group()
         else:
-            # print(f"Operation amount {money} could not be found using phone number as reference")
             return None
 
-    # def op_card(self):
-    #     tables = self.soup.find_all('td')
-    #     for card in DB(db_table='card').all_cards():
-    #         for element in tables:
-    #             if re.search("0180*931987", element.text):
-    #                 if f"*{card['number']}" in element.text:
-    #                     return card['id']
-    #                     # return card
+    # def op_account(self):
+    #     accounts = DB(db_table='account').all_accounts()
+    #     cards = DB(db_table='card').all_cards()
+    #     for element in self.soup:
+    #         for account in accounts:
+    #             if f"*{account['number']}" in element.text:
+    #                 return account
+    #         for card in cards:
+    #             if f"*{card['number']}" in element.text:
+    #                 return card
     #     else:
-    #         # print("Operation card could not be found using known card numbers as reference")
     #         return None
 
     def op_account(self):
-        accounts = DB(db_table='account').all_accounts()
-        cards = DB(db_table='card').all_cards()
+        accounts = DB(db_table='account')
+        cards = DB(db_table='card')
         for element in self.soup:
-            for account in accounts:
+            for account in accounts.all_accounts():
                 if f"*{account['number']}" in element.text:
                     return account
-            for card in cards:
+            for card in cards.all_cards():
                 if f"*{card['number']}" in element.text:
-                    return card
+                    if card['type'] == 'debit':
+                        account = accounts.account_from_id(card['account_id'])
+                        return account
+                    else:
+                        return card
         else:
-            # print("Operation account could not be found using known account numbers as reference")
             return None
+
 
     def op_entity_bank_payment(self):
         for item in self.soup:
@@ -299,7 +116,6 @@ class MailOperation:
         return None
 
     def op_entity(self):
-        # tables = self.soup.find_all('td')
         entity = None
 
         for item in self.soup:
@@ -307,10 +123,7 @@ class MailOperation:
                 return self.op_entity_bank_payment()
 
             elif re.search("0180*931987", item.text):
-            # elif self.operation_string in item.text:
                 if self.op_type() == 'manual payment':                                        # pago manual
-                    # text_w_money = item.text.lower().split(self.operation_string)[1]
-                    # entity = text_w_money.split('realizo abono')[0].strip()
                     return self.op_entity_manual_payment()
                 elif self.op_type() == 'transfer':                                          # transferencia
                     text_wo_money = item.text.lower().split(self.op_amount())[1]
@@ -327,10 +140,6 @@ class MailOperation:
                         time = re.search("\d{2}\:\d{2}", text_w_timedate).group()
                         entity = text_w_timedate.split(time)[0].replace('. hora', '').strip()
                 elif self.op_type() == 'expense':
-                    # if 'amazon' in item.text.lower():
-                    #     print("amazon string")
-                    #     print(self.operation_string)
-                    #     print(item.text.lower().split(self.operation_string)[1])
                     text_w_money = item.text.lower().split(self.operation_string)[1]
                     if re.search("\d{2}\:\d{2}", text_w_money):
                         if 'desde cta' in text_w_money:
@@ -339,7 +148,7 @@ class MailOperation:
                         else:
                             text_w_timedate = text_w_money.split(f"{self.op_amount()} en")[1]         # compras habituales
                             time = re.search("\d{2}\:\d{2}", text_w_timedate).group()
-                            entity = text_w_timedate.split(time)[0].strip()
+                            entity = text_w_timedate.split(time)[0].strip().rstrip('.')
                 elif self.op_type() == 'recurring bill':
                     text_w_money = item.text.lower().split(self.operation_string)[1]
                     if re.search("\d{2}/\d{2}/\d{4}", text_w_money):               # debito automatico (no time, only date)
@@ -347,11 +156,7 @@ class MailOperation:
                         text_no_extras = text_no_extras.split(' (')[0]
                         entity = text_no_extras.replace('  ', ' ')
                 else:
-                    # print(self.op_type())
-                    # print(f"Debug data: \ndb_table: {self.db_table}\nop_type: {self.op_type()}\nop_account: {self.op_account()}\nop_amount: {self.op_amount()}")
-                    # raise ValueError("Could not find op_type to grab entity")
                     print("Could not find op_type to grab entity")
-                # entity = string.title()
             if entity is not None:
                 entity = entity.title()
         return entity
