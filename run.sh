23fcd6995b9cf19924628473bddd867443f36773
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-26 17:36:07 -0500

    Revert "Revert "update run.sh""
    This reverts commit 260c1a9b81a0e742498a9c4e136033732bfa1e36.

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 mkdir -p logs
 cd /home/sergio/scripts/bancolombia_mails/
+find logs -type f -mtime +30 -delete
 . ./env/bin/activate
 /home/sergio/scripts/bancolombia_mails/env/bin/python3 /home/sergio/scripts/bancolombia_mails/main.py >> logs/bancolombia_mails-"`date +"%Y-%m-%d_%H.%M.%S"`".log 2>&1
 deactivate
